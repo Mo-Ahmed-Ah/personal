83970ae39e9166e4da03540d1c75204f0def954d
Ahmed Mohamed <ahmed0033ahmed.mo@gmail.com>
2024-11-12 04:47:27 +0200

    finshed my profile

---
+++ script/script.js
@@ -0,0 +1,73 @@
+// تحديد المتغيرات
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+// التحكم في الأيقونة عند النقر
+menuIcon.onclick = () => {
+  // تبديل حالة الأيقونة عند النقر
+  menuIcon.classList.toggle("fa-xmark");
+  navbar.classList.toggle("active");
+};
+
+/*======================================== scroll sections active ========================================*/
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
+
+window.onscroll = () => {
+  // تحديد الجزء الحالي في الصفحة لتفعيل الرابط المناسب
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((link) => {
+        link.classList.remove("active");
+        document
+          .querySelector(`header nav a[href*="${id}"]`)
+          .classList.add("active");
+      });
+    }
+  });
+
+  /*======================================== sticky navbar ========================================*/
+  let header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 100);
+  /*======================================== remove toggle icon and navbar when click navbar link (scroll) ======================================== */
+  menuIcon.classList.remove("fa-xmark");
+  navbar.classList.remove("active");
+};
+
+/*======================================== scroll reveal ========================================*/
+// تفعيل تأثير ScrollReveal
+ScrollReveal({
+  distance: "80px",
+  duration: 2000,
+  delay: 200,
+});
+
+ScrollReveal().reveal(".home-content, .heading", { origin: "top" });
+ScrollReveal().reveal(
+  ".home-image, .services-container, .protofolio-box, .contact form",
+  { origin: "bottom" }
+);
+ScrollReveal().reveal(".home-content h1 , .about-img , .contact form *", {
+  origin: "left",
+});
+ScrollReveal().reveal(".home-content p, .about-content p ", {
+  origin: "right",
+});
+
+/*======================================== typed js ========================================*/
+const typed = new Typed(".multiple-text", {
+  strings: [
+    "Front-End Developer",
+    "Back-End Developer",
+    "Full-Stack Developer",
+  ],
+  typeSpeed: 70,
+  backSpeed: 70,
+  backDelay: 1000,
+  loop: true,
+});
